7d853787a74732f13697e19eb28c29927b3d829f
R.Noei <reza.noei@chmail.ir>
2025-09-29 16:42:31 +0330

    Add projects.

---
+++ WebScanDriver.Service/Controllers/DevicesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace WebScanDriver.Service.Controllers
+{
+    public class DevicesController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Devices")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello from TopShelf Web API (.NET Framework)!");
+        }
+    }
+}
